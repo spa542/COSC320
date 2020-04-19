2cd2ba7a5373ee37fcf53725688b8d66dc4eb661
Ryan Rosiak <ryan11291129@gmail.com>
2020-04-19 14:55:13 -0400

    Graphs are working, need to implement bfs and readme and zip

--- COSC-320/Lab-10/Graph.cpp
+++ COSC-320/Lab-10/Graph.cpp
@@ -5,7 +5,14 @@
    Adds a vertex to the graph 
 */
 void Graph::addVertex(int addMe) {
-	
+	std::map<int, std::vector<int>>::iterator it;
+	it = vertices.find(addMe);
+	if (it != vertices.end()) {
+		std::cout << "Vertex " << addMe << " is already in the graph" << std::endl;
+		return;
+	}
+	std::vector<int> edge;
+	vertices.insert(std::pair<int, std::vector<int>>(addMe, edge));
 }
 
 /*
@@ -13,21 +20,43 @@ void Graph::addVertex(int addMe) {
    Adds an undirected edge to the graph
 */
 void Graph::addEdge(int first, int second) {
-	
+	if (first == second) {
+		std::cout << "No self edges" << std::endl;
+		return;
+	}
+	std::map<int, std::vector<int>>::iterator it;
+	it = vertices.find(first);
+	if (it == vertices.end()) {
+		std::cout << first << " vertex not found" << std::endl;
+		return;
+	}
+	it = vertices.find(second);
+	if (it == vertices.end()) {
+		std::cout << second << " vertex not found" << std::endl;
+		return;
+	}
+	vertices[first].push_back(second);
+	vertices[second].push_back(first);
 }
 
 /*
    print Function:
-   Prints the adjacency list of each vertext to show the structure
+   Prints the adjacency list of each vertex to show the structure
 */
 void Graph::print() {
-	
+	for (std::map<int, std::vector<int>>::iterator it = vertices.begin(); it != vertices.end(); ++it) {
+		std::cout << it->first << " is connected to these vertices..." << std::endl;
+		for (std::vector<int>::iterator vt = it->second.begin(); vt != it->second.end(); ++vt) {
+			std::cout << *vt << ", ";	
+		}
+		std::cout << std::endl;
+	}
 }
 
 /*
    printBfs Function:
    Prints the vertices discovered by a BFS, starteing at a given vertex
 */
-void Graph::printBfs(int me) {
+void Graph::printBfs(int startPoint) {
 
 }
--- COSC-320/Lab-10/main.cpp
+++ COSC-320/Lab-10/main.cpp
@@ -1,12 +1,37 @@
-#include<iostream>
-#include<stdio.h>
-#include "Graph.h"
-#include<vector>
-#include<map>
+#include<iostream> // cout, endl
+#include<stdio.h> // printf
+#include"Graph.h" // Graph class
+#include<vector> // std::vector
+#include<map> // std::map
 
 int main() {
 
+	Graph g;
 	
+	std::cout << "Inserting vertices..." << std::endl;
+	g.addVertex(2);
+	g.addVertex(3);
+	g.addVertex(5);
+	g.addVertex(-2);
+
+	g.print();
+
+	std::cout << "Testing the insert of a vertex that is already inserted..." << std::endl;
+	g.addVertex(2);
+
+	std::cout << "Inserting edges..." << std::endl;
+	g.addEdge(2, 3);
+	g.addEdge(3, 5);
+	g.addEdge(-2, 5);
+
+	g.print();
+
+	std::cout << "Testing out adding an edge to itself..." << std::endl;
+	g.addEdge(5, 5);
+
+	std::cout << "Testing out adding an edge to vertices that may not be in the map..." << std::endl;
+	g.addEdge(5, 0);
+	g.addEdge(-2, 10);
 
 	return 0;	
 }
